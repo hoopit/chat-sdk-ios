6045c5d99d974730b9cc075dc29087306b241458
Kiran Thakur <kiran.thakur@IsTeams-Mac-mini-10.local>
2019-09-25 14:39:42 +0530

    Added condition while controller pop on leaving chat

--- ChatSDKUI/Classes/Components/Chat View/BChatViewController.m
+++ ChatSDKUI/Classes/Components/Chat View/BChatViewController.m
@@ -73,10 +73,9 @@
 -(void)leaveChat {
     [BChatSDK.core deleteThread:_thread];
     [BChatSDK.core leaveThread:_thread];
-    // if true, back was pressed
-//    if (![self.navigationController.viewControllers indexOfObject:self]==NSNotFound) {
+    if (!self.isMovingFromParentViewController) {
         [self.navigationController popViewControllerAnimated:true];
-//    }
+    }
 }
 
 -(void) openInviteScreen {
